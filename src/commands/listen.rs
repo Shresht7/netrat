a9c498001d405d6f5d6eb1e0a18a499504e36be8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-23 02:56:39 +0530

    Add the `--host` option for the `listen` command
    This allows the user to bind the TcpListener to something like `0.0.0.0` in addition to `127.0.0.1`.

--- src/commands/listen.rs
+++ src/commands/listen.rs
@@ -9,13 +9,24 @@ use crate::connection;
 pub struct Listen {
     /// The local address port to bind to (e.g. 4321)
     port: u16,
+
+    /// The local interface to bind to (e.g. 0.0.0.0 for all interfaces, defaults to 127.0.0.1)
+    #[arg(long, default_value = "127.0.0.1")]
+    host: String,
 }
 
 impl Listen {
     /// Run the application in server mode (i.e. listens for incoming connections)
     pub fn run(&self) -> std::io::Result<()> {
+        // Use the host provided, or if "localhost", resolve to "127.0.0.1"
+        let host = if self.host == "localhost" {
+            "127.0.0.1"
+        } else {
+            &self.host
+        };
+
         // Form the local address to use for the server
-        let address = format!("127.0.0.1:{}", self.port);
+        let address = format!("{}:{}", host, self.port);
 
         // Setup the [`TcpListener`] and bind it to the address
         let listener = TcpListener::bind(&address)?;
